01eb69c834907bdfeb521a01711aabe930b3fd26
AnaNunes3 <nunes.silva13@escola.pr.gov.br>
2026-03-27 07:56:54 -0300

    update

---
+++ aulas/27032026.js
@@ -0,0 +1,10 @@
+// Criar Array
+
+const filaNumber =[1,2,3,4,5,6];
+
+const filaStringAmigos= ["Ana Julia", "Bia", "Maisa", "Yzadora", "Jennifer"," Malu"]
+
+function LerAmigo(filaStringAmigos){
+    console.log ('Voce')
+
+}
